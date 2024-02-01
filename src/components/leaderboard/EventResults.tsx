378fb2361a8856f71d33456a7b9b755f6307d5a2
racollette <racollette@gmail.com>
2024-02-01 15:13:00 -0500

    leaderboard result tables

--- src/components/leaderboard/EventResults.tsx
+++ src/components/leaderboard/EventResults.tsx
@@ -9,7 +9,9 @@ import {
   type Wallet,
 } from "@prisma/client";
 import Image from "next/image";
+import { truncateAccount } from "~/utils/addresses";
 import { extractProfileFromUser } from "~/utils/wallet";
+import { handleUserPFPDoesNotExist } from "~/utils/images";
 
 type EventResultsProps = {
   event: Event & {
@@ -27,31 +29,67 @@ type EventResultsProps = {
 
 export const EventResults = ({ event }: EventResultsProps) => {
   return (
-    <div>
-      <div>{event.name}</div>
-      <table className="w-full table-auto">
+    <div className="flex flex-col items-center justify-center gap-4 rounded-lg bg-purple-900 p-6 font-clayno">
+      <div className="text-xl font-extrabold">{event.name}</div>
+      <table className="w-full table-auto border-separate border-spacing-2">
         <thead>
           <tr>
-            <th className="px-4 py-2">Username</th>
-            <th className="px-4 py-2">Score</th>
+            <th className="px-4 py-2 text-left">Rank</th>
+            <th className="px-4 py-2 text-left">Player</th>
+            <th className="px-4 py-2 text-left">Clayno</th>
+            <th className="px-4 py-2 text-left">Time (s)</th>
           </tr>
         </thead>
         <tbody>
-          {event.results.map((result) => {
-            const { username } = extractProfileFromUser(result.user);
+          {event.results.map((result, idx) => {
+            const { username, userPFP, userHandle } = extractProfileFromUser(
+              result.user
+            );
+            console.log(userPFP);
+            console.log(result);
             return (
               <tr key={result.id}>
-                <td className="flex items-center gap-4">
-                  <Image
-                    src={`https://prod-image-cdn.tensor.trade/images/slug=claynosaurz/400x400/freeze=false/${result?.dino?.pfp}`}
-                    alt={username as string}
-                    width={50}
-                    height={50}
-                    className="rounded-full"
-                  />
-                  <div>{username}</div>
+                <td className="px-4">
+                  <div className="max-w-xs">{idx + 1}</div>
                 </td>
-                <td>{result.score}</td>
+                <td className="px-4">
+                  <div className="flex items-center gap-2">
+                    <Image
+                      src={
+                        userPFP ??
+                        `https://ui-avatars.com/api/?name=${result.user.defaultAddress}&background=random`
+                      }
+                      alt={username as string}
+                      width={32}
+                      height={32}
+                      className="rounded-full"
+                      onError={handleUserPFPDoesNotExist}
+                    />
+                    <div className="max-w-xs">
+                      {" "}
+                      {userHandle
+                        ? userHandle
+                        : truncateAccount(result.user.defaultAddress)}
+                    </div>
+                  </div>
+                </td>
+                <td className="px-4">
+                  <div className="flex items-center gap-2">
+                    <Image
+                      src={`https://prod-image-cdn.tensor.trade/images/slug=claynosaurz/400x400/freeze=false/${result?.dino?.gif}`}
+                      alt={result?.dino?.name as string}
+                      width={50}
+                      height={50}
+                      className="rounded-xl"
+                    />
+                    {/* <div className="max-w-xs">
+                      {result?.dino?.name.split(" ")[1]}
+                    </div> */}
+                  </div>
+                </td>
+                <td className="px-4">
+                  <div className="max-w-xs">{result.score}</div>
+                </td>
               </tr>
             );
           })}
