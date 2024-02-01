f36c67e835a316c0b53f3223687a41b686e29795
racollette <racollette@gmail.com>
2024-02-01 15:13:24 -0500

    create game link

--- src/pages/events/index.tsx
+++ src/pages/events/index.tsx
@@ -6,10 +6,35 @@ import { extractProfileFromUser } from "~/utils/wallet";
 import LoginModal from "~/components/LoginModal";
 import Inventory from "~/components/inventory/Inventory";
 import Header from "~/components/Header";
+import useGame from "~/stores/useGame";
+import { set } from "zod";
+import { useEffect, useState } from "react";
+import { type Dino } from "@prisma/client";
 
 export default function Home() {
   const { user } = useUser();
   const { username } = extractProfileFromUser(user);
+  const [selected, setSelected] = useState<Dino | null>(null);
+
+  const userId = useGame((state) => state.userId);
+  const dino = useGame((state) => state.dino);
+
+  console.log(userId);
+  console.log(dino);
+
+  const [gameLink, setGameLink] = useState<string>("");
+
+  useEffect(() => {
+    setSelected(dino);
+    if (dino?.attributes?.species && dino?.attributes?.mood && dino?.name) {
+      const encodedSpecies = btoa(dino.attributes.species.toLowerCase());
+      const encodedMood = btoa(dino.attributes.mood.toLowerCase());
+      const encodedNumber = btoa(dino.name.split("#")[1]);
+      setGameLink(
+        `/slalom?species=${encodedSpecies}&mood=${encodedMood}&number=${encodedNumber}`
+      );
+    }
+  }, [dino]);
 
   return (
     <>
@@ -30,7 +55,27 @@ export default function Home() {
             {user ? (
               <div className="flex flex-col items-center justify-center gap-2 p-4">
                 <div className="font-clayno text-3xl">Select your clayno!</div>
-                <Inventory username={username} />
+                {gameLink && (
+                  <div className="mt-2 flex w-full justify-center">
+                    <Link
+                      href={gameLink}
+                      className="w-full animate-pulse cursor-pointer justify-center rounded-lg bg-purple-800 px-4 py-2 text-center font-clayno text-2xl hover:scale-105"
+                    >
+                      <div className="flex flex-row items-center justify-center gap-2">
+                        <Image
+                          src={`https://prod-image-cdn.tensor.trade/images/slug=claynosaurz/400x400/freeze=false/${selected?.pfp}`}
+                          alt={`${selected?.name}`}
+                          width={40}
+                          height={40}
+                          className="rounded-full"
+                        />
+                        Ski!
+                      </div>
+                    </Link>
+                  </div>
+                )}
+
+                <Inventory username={username} userId={user.id} />
               </div>
             ) : (
               <div className="flex w-full flex-col items-center justify-center gap-2 pb-6">
