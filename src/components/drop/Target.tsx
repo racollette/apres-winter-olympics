792c59b74b67663b0678bc6a7a4b96af489327a3
racollette <racollette@gmail.com>
2024-02-11 16:37:55 -0500

    add target texture

--- src/components/drop/Target.tsx
+++ src/components/drop/Target.tsx
@@ -7,16 +7,18 @@ import { useControls } from "leva";
 const Target = () => {
   const targetRef = useRef<THREE.Mesh | null>(null);
 
-  const matcap4 = useTexture("/textures/68049F_C90DE6_A404CF_B304DC-256px.png");
+  // const matcap4 = useTexture("/textures/68049F_C90DE6_A404CF_B304DC-256px.png");
+  const targetTexture = useTexture("/textures/target.png");
 
   return (
     <>
       <group rotation={[0, 0, 0]} position={[0, 0, 0]}>
-        <RigidBody type="fixed" restitution={0} friction={0}>
+        <RigidBody type="fixed" restitution={0.1} friction={0.005}>
           <mesh ref={targetRef} position={[0, -1, 0]} receiveShadow>
             <cylinderGeometry args={[100, 100, 1]} />
             {/* <meshBasicMaterial color="white" /> */}
-            <meshMatcapMaterial map={matcap4} />
+            {/* <meshMatcapMaterial map={matcap4} /> */}
+            <meshStandardMaterial map={targetTexture} />
           </mesh>
         </RigidBody>
       </group>
