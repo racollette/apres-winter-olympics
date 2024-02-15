f4a0feb2b763a83637a87029c2c8b178a3eb4379
racollette <racollette@gmail.com>
2024-02-14 23:28:32 -0500

    refactor events page

--- src/pages/events/index.tsx
+++ src/pages/events/index.tsx
@@ -1,52 +1,9 @@
-import Link from "next/link";
 import Metatags from "~/components/MetaTags";
-import Image from "next/image";
-import { useUser } from "~/hooks/useUser";
-import { extractProfileFromUser } from "~/utils/wallet";
-import LoginModal from "~/components/LoginModal";
-import Inventory from "~/components/inventory/Inventory";
 import Header from "~/components/Header";
-import useGame from "~/stores/useGame";
-import { set } from "zod";
-import { useEffect, useState } from "react";
-import { type Dino } from "@prisma/client";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
-  const { user } = useUser();
-  const { username } = extractProfileFromUser(user);
-  const [selected, setSelected] = useState<Dino | null>(null);
-
-  const userId = useGame((state) => state.userId);
-  const dino = useGame((state) => state.dino);
-
-  console.log(userId);
-  console.log(dino);
-
-  const [gameLink, setGameLink] = useState<string>("");
-
-  useEffect(() => {
-    setSelected(dino);
-    if (dino?.attributes?.species && dino?.attributes?.mood && dino?.name) {
-      const encodedSpecies = dino.attributes.species.toLowerCase();
-      const encodedMood = dino.attributes.mood.toLowerCase();
-      const claynoNumber = dino?.name.split("#")[1] ?? "10176";
-      const encodedNumber = claynoNumber;
-
-      if (
-        dino?.attributes?.species === "Spino" ||
-        dino?.attributes?.species === "Para" ||
-        dino?.attributes?.species === "Dactyl"
-      ) {
-        setGameLink(`/slalom?species=rex&mood=excited&number=5630`);
-        return;
-      }
-
-      setGameLink(
-        `/slalom?species=${encodedSpecies}&mood=${encodedMood}&number=${encodedNumber}`
-      );
-    }
-  }, [dino]);
-
   return (
     <>
       <Metatags
@@ -54,68 +11,167 @@ export default function Home() {
         description="2024 Apres Claynosaurz Winter Olympics. Sponsored by Apres Mountain Lodge and Clayno.club"
       />
       <Header />
-      <main
-        className="text-white"
-        // style={{
-        //   background: `url('/textures/apres_triangles.png')`,
-        //   backgroundSize: `100%`,
-        // }}
-      >
+      <main className="text-white">
         <div className="flex flex-col items-center justify-center gap-12 p-4 pb-16">
-          <section className="flex w-full flex-col items-center justify-center gap-8 rounded-xl bg-blue-950 p-4 md:w-3/4">
-            {user ? (
-              <div className="flex flex-col items-center justify-center gap-2 p-4">
-                <div className="font-clayno text-3xl">Select your clayno!</div>
-                {gameLink && (
-                  <div className="mt-2 flex w-full justify-center">
-                    <Link
-                      href={gameLink}
-                      className="w-4/5 animate-pulse cursor-pointer justify-center rounded-lg bg-purple-800 px-4 py-2 text-center font-clayno text-2xl hover:scale-105"
-                    >
-                      <div className="flex flex-row items-center justify-center gap-2">
-                        <Image
-                          src={`https://prod-image-cdn.tensor.trade/images/slug=claynosaurz/400x400/freeze=false/${selected?.pfp}`}
-                          alt={`${selected?.name}`}
-                          width={40}
-                          height={40}
-                          className="rounded-full"
-                        />
-                        Ski!
-                      </div>
-                    </Link>
-                  </div>
-                )}
-
-                <Inventory username={username} userId={user.id} />
-              </div>
-            ) : (
-              <div className="flex w-full flex-col items-center justify-center gap-2 pb-6">
-                <div className="py-4 font-clayno text-xl md:text-3xl">
-                  Sign in to begin!
-                </div>
-                <div className="flex w-full flex-row justify-center gap-2">
-                  <div className="relative aspect-[1/1] w-1/2 md:w-1/4">
+          <div className="flex w-3/4 flex-col items-center justify-center gap-2 rounded-xl bg-blue-950 p-4">
+            <h1 className="p-4 font-clayno text-4xl">Event Directory</h1>
+            <div className="flex flex-col items-center justify-center gap-4">
+              <EventListing
+                number={1}
+                name="Apres Ski Games"
+                startDate="Monday Feb. 26th"
+                endDate="Friday March 1st"
+                description="Compete in a variety of mini-games with your clayno!"
+              >
+                <div className="mt-4 flex w-full flex-row items-center justify-between gap-2">
+                  <Link
+                    href="/events/select?event=slalom"
+                    className="relative flex aspect-square cursor-pointer flex-col items-center justify-center gap-2 rounded-lg bg-purple-950 p-2 hover:scale-105 hover:bg-cyan-500"
+                  >
                     <Image
                       src="/images/lodge.jpeg"
-                      alt="The Lodge"
-                      fill
-                      className="rounded-lg"
+                      alt="Time Trial"
+                      width={200}
+                      height={200}
+                      className="rounded-md"
                     />
-                  </div>
-                  <div className="relative w-1/2 md:w-1/4">
+                    <p className="font-bold">Time Trial</p>
+                  </Link>
+                  <Link
+                    href="/events/select?event=drop"
+                    className="flex  cursor-pointer flex-col items-center justify-center gap-2 rounded-lg bg-purple-950 p-2 hover:scale-105 hover:bg-cyan-500"
+                  >
                     <Image
-                      src="/images/relax.jpeg"
-                      alt="Apres Ski"
-                      fill
-                      className="rounded-lg"
+                      src="/images/lodge.jpeg"
+                      alt="Taxi Training"
+                      width={200}
+                      height={200}
+                      className="rounded-md"
                     />
-                  </div>
+                    <p className="font-bold">Taxi Training</p>
+                  </Link>
+                  <Link
+                    href="/events/select?event=delivery"
+                    className="flex cursor-pointer flex-col items-center justify-center gap-2 rounded-lg bg-purple-950 p-2 hover:scale-105 hover:bg-cyan-500"
+                  >
+                    <Image
+                      src="/images/lodge.jpeg"
+                      alt="Delivery"
+                      width={200}
+                      height={200}
+                      className="rounded-md"
+                    />
+                    <p className="font-bold">Delivery</p>
+                  </Link>
                 </div>
-              </div>
-            )}
-          </section>
+              </EventListing>
+              <EventListing
+                number={2}
+                name="Meme Contest"
+                startDate="Tuesday Feb. 27th"
+                links={[
+                  {
+                    location: "Submissions",
+                    url: "https://x.com/ApresMountLodge",
+                  },
+                  { location: "Voting", url: "https://x.com/ApresMountLodge" },
+                ]}
+              ></EventListing>
+              <EventListing
+                number={3}
+                name="Snowfall Prediction"
+                startDate="Wednesday Feb. 28th"
+                links={[
+                  { location: "Guesses", url: "https://x.com/ApresMountLodge" },
+                ]}
+              ></EventListing>
+              <EventListing
+                number={4}
+                name="Scavenger Hunt"
+                startDate="Thursday Feb. 29th"
+                links={[
+                  {
+                    location: "Submissions",
+                    url: "https://x.com/ApresMountLodge",
+                  },
+                ]}
+              ></EventListing>
+              <EventListing
+                number={5}
+                name="Degen Trivia"
+                startDate="Friday March 1st"
+                links={[
+                  {
+                    location: "Submissions",
+                    url: "https://x.com/ApresMountLodge",
+                  },
+                ]}
+              ></EventListing>
+            </div>
+          </div>
+          {/* <Select /> */}
         </div>
       </main>
     </>
+  );
+}
+
+type Link = {
+  location: string;
+  url: string;
+};
+
+type EventListingProps = {
+  number: number;
+  name: string;
+  startDate?: string;
+  endDate?: string;
+  description?: string;
+  links?: Link[];
+};
+
+function EventListing({
+  number,
+  name,
+  startDate,
+  endDate,
+  description,
+  links,
+  children, // Include children prop
+}: EventListingProps & { children?: React.ReactNode }) {
+  return (
+    <div className="w-full rounded-lg bg-purple-900 p-4">
+      <div className="text-center font-clayno text-2xl">
+        {`#${number}:`} {name}
+      </div>
+      <div className="flex flex-col items-center justify-center gap-2 px-4 pb-2">
+        <div className="text-sm">
+          {startDate} {endDate && `- ${endDate}`}
+        </div>
+        {description && (
+          <div className="text-md font-semibold">{description}</div>
+        )}
+        {links && (
+          <div className="flex flex-row items-center justify-center gap-4 text-sm font-semibold">
+            <div className="rounded-md bg-fuchsia-700 px-2 py-1 italic text-neutral-300">
+              Links
+            </div>
+            {links.map((link) => (
+              <a
+                key={link.url}
+                href={link.url}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-cyan-400 hover:text-cyan-500"
+              >
+                {link.location}
+              </a>
+            ))}
+          </div>
+        )}
+        {/* Render children if provided */}
+        {children}
+      </div>
+    </div>
   );
 }
