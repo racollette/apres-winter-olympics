03097ae891f9fbc59b5a7ac78a55e4a773381ff2
racollette <racollette@gmail.com>
2024-02-19 20:47:22 -0500

    create ledge positions

--- src/components/delivery/Mountain.tsx
+++ src/components/delivery/Mountain.tsx
@@ -17,6 +17,50 @@ import {
 import useGame from "../../stores/useGame";
 import { MeshPhysicalMaterial } from "three";
 
+const ledgePositions = [
+  { x: 0, y: 0, z: -50 },
+  { x: 12, y: 4, z: -50 },
+  { x: 24, y: 8, z: -50 },
+  { x: 36, y: 12, z: -50 },
+  { x: 48, y: 16, z: -50 },
+  { x: 60, y: 20, z: -50.5 },
+  { x: 72, y: 24, z: -50.5 },
+  { x: 60, y: 28, z: -51 },
+  { x: 48, y: 32, z: -51 },
+  { x: 36, y: 36, z: -54.5 },
+  { x: 24, y: 40, z: -51.5 },
+  { x: 12, y: 44, z: -52 },
+  { x: 0, y: 48, z: -52 },
+  { x: -12, y: 52, z: -55.5 },
+  { x: -24, y: 56, z: -56.5 },
+  { x: -36, y: 60, z: -57 },
+  { x: -48, y: 64, z: -55 },
+  { x: -60, y: 68, z: -55.5 },
+  { x: -72, y: 72, z: -58 },
+  { x: -60, y: 76, z: -58 },
+  { x: -48, y: 80, z: -57 },
+  { x: -36, y: 84, z: -59 },
+  { x: -24, y: 88, z: -59 },
+  { x: -12, y: 92, z: -59 },
+  { x: 0, y: 96, z: -60 },
+  { x: 12, y: 100, z: -60 },
+  { x: 24, y: 104, z: -59 },
+  { x: 36, y: 108, z: -62 },
+  { x: 48, y: 112, z: -60 },
+  { x: 60, y: 116, z: -62 },
+  { x: 72, y: 120, z: -63 },
+  { x: 60, y: 124, z: -63 },
+  { x: 48, y: 128, z: -64 },
+  { x: 36, y: 132, z: -64 },
+  { x: 24, y: 136, z: -64 },
+  { x: 12, y: 140, z: -64 },
+  { x: 0, y: 144, z: -64 },
+  { x: -12, y: 148, z: -65 },
+  { x: -24, y: 152, z: -65 },
+  { x: -36, y: 156, z: -65 },
+  { x: -48, y: 160, z: -65 },
+];
+
 const Mountain = () => {
   const slopeRef = useRef<THREE.Mesh | null>(null);
 
@@ -25,6 +69,17 @@ const Mountain = () => {
   // useFrame((state, delta) => {
   //   // You can add animation logic here if needed
   // });
+
+  const colorTexture = useTexture("/textures/snow_02_diff_4k.jpg");
+  colorTexture.colorSpace = THREE.SRGBColorSpace;
+  colorTexture.wrapS = THREE.RepeatWrapping;
+  colorTexture.wrapT = THREE.RepeatWrapping;
+  colorTexture.repeat.x = 4;
+  colorTexture.repeat.y = 4;
+  // const displacementTexture = useTexture("/textures/snow_02_disp_4k.jpg");
+  const normalTexture = useTexture("/textures/snow_02_nor_gl_4k.jpg");
+  const aoTexture = useTexture("/textures/Snow_002_OCC.jpg");
+  const roughnessTexture = useTexture("/textures/Snow_002_ROUGH.jpg");
 
   const cliff = useGLTF("/models/cliff.glb");
   const ledge = useGLTF("/models/ledge.glb");
@@ -36,25 +91,46 @@ const Mountain = () => {
         <RigidBody type="fixed" restitution={0.2} friction={1}>
           <mesh ref={slopeRef} position={[0, -1, -240]} receiveShadow>
             <boxGeometry args={[500, 1, 500]} />
-            <meshBasicMaterial color="white" />
+            <meshStandardMaterial
+              map={colorTexture}
+              // displacementMap={displacementTexture}
+              aoMap={aoTexture}
+              roughnessMap={roughnessTexture}
+              normalMap={normalTexture}
+            />
           </mesh>
         </RigidBody>
       </group>
 
       <group scale={[15, 10, 10]}>
+        <primitive object={cliff.scene} position={[0, 6, -11]} />
         <RigidBody type="fixed">
-          <primitive object={cliff.scene} position={[0, 6, -10]} />
-          <CuboidCollider args={[10, 10, 10]} />
+          <CuboidCollider
+            rotation={[-0.12, 0, 0]}
+            position={[0, 7, -11.75]}
+            args={[6.5, 7.5, 0.9]}
+          />
         </RigidBody>
       </group>
 
-      <group position={[0, 0, -40]}>
-        <RigidBody type="fixed" position={[0, 2, -50]}>
-          <mesh position={[0, 0, 0]}>
-            <boxGeometry args={[8, 1.5, 8]} />
-            <meshBasicMaterial color="blue" />
-          </mesh>
-        </RigidBody>
+      <group position={[0, 0, -50]}>
+        {ledgePositions.map((position, index) => (
+          <RigidBody
+            type="fixed"
+            position={[position.x, position.y, position.z]}
+          >
+            <mesh position={[0, 0, 0]}>
+              <boxGeometry args={[8, 1, 8]} />
+              <meshStandardMaterial
+                map={colorTexture}
+                // displacementMap={displacementTexture}
+                aoMap={aoTexture}
+                roughnessMap={roughnessTexture}
+                normalMap={normalTexture}
+              />
+            </mesh>
+          </RigidBody>
+        ))}
       </group>
     </>
   );
