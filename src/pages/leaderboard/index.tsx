d871705724a50605f45507709c08d6bb75432ad2
racollette <racollette@gmail.com>
2024-02-23 20:25:14 -0500

    add scoring info

--- src/pages/leaderboard/index.tsx
+++ src/pages/leaderboard/index.tsx
@@ -8,8 +8,6 @@ import { api } from "~/utils/api";
 import { EventResults } from "~/components/leaderboard/EventResults";
 
 export default function Leaderboard() {
-  const { user } = useUser();
-
   const { data: events, isLoading } = api.leaderboard.getAllEvents.useQuery({
     competitionId: 1,
   });
@@ -23,10 +21,27 @@ export default function Leaderboard() {
       <Header />
       <main className="text-white">
         <div className="flex flex-col items-center justify-center gap-4 p-1 pb-16 md:gap-12 md:p-4">
-          <section className="flex w-full flex-col items-center justify-center gap-4 rounded-xl bg-blue-950 p-4 md:w-3/4 md:gap-8">
-            <h1 className="pt-2 font-clayno text-2xl font-extrabold md:text-3xl">
-              Event Leaderboards
-            </h1>
+          <section className="flex w-full flex-col items-center justify-center gap-4 rounded-xl bg-blue-950 p-4 md:w-3/4">
+            <div className="flex w-full flex-col items-center justify-center gap-2 md:w-[600px]">
+              <h1 className="pt-2 font-clayno text-2xl font-extrabold md:text-3xl">
+                Event Leaderboards
+              </h1>
+              <p className="text-center">
+                Each event contributes points to the{" "}
+                <Link href="/podium" className="text-sky-500">
+                  overall competition leaderboard
+                </Link>
+                . The top 3 will make the podium!
+              </p>
+              <p className="text-md mt-4 font-clayno font-bold underline">
+                Scoring Critera
+              </p>
+              <p className="text-center">
+                Points will be awarded to the top 10 finishers in each event (10
+                for 1st, 9 for 2nd, etc.), but points are doubled for the Apres
+                Ski Games (20 for 1st, 18 for 2nd, etc.).
+              </p>
+            </div>
             <div className="flex w-full flex-col items-center  justify-center gap-4 rounded-lg bg-purple-900 p-2 md:w-[650px] md:p-6">
               <h2 className="font-clayno text-xl font-extrabold">
                 #1: Apres Ski Games
@@ -41,21 +56,25 @@ export default function Leaderboard() {
               <h2 className="font-clayno text-xl font-extrabold">
                 #2: Meme Contest
               </h2>
+              <p className="text-sm italic">Results pending</p>
             </div>
             <div className="flex w-full flex-col items-center justify-center gap-4 rounded-lg bg-purple-900 p-2 md:w-[650px] md:p-4">
               <h2 className="font-clayno text-xl font-extrabold">
                 #3: Snowfall Prediction
               </h2>
+              <p className="text-sm italic">Results pending</p>
             </div>
             <div className="flex w-full flex-col items-center justify-center gap-4 rounded-lg bg-purple-900 p-2 md:w-[650px] md:p-4">
               <h2 className="font-clayno text-xl font-extrabold">
                 #4: Scavenger Hunt
               </h2>
+              <p className="text-sm italic">Results pending</p>
             </div>
             <div className="flex w-full flex-col items-center justify-center gap-4 rounded-lg bg-purple-900 p-2 md:w-[650px] md:p-4">
               <h2 className="font-clayno text-xl font-extrabold">
                 #5: Degen Trivia
               </h2>
+              <p className="text-sm italic">Results pending</p>
             </div>
           </section>
         </div>
