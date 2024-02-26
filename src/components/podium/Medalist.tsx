6e79252b5937aa8f880f06f8bb71213bb512d279
racollette <racollette@gmail.com>
2024-02-26 18:53:26 -0500

    clean  up podium names

--- src/components/podium/Medalist.tsx
+++ src/components/podium/Medalist.tsx
@@ -2,7 +2,8 @@ import React, { useRef, useEffect, useState } from "react";
 import { useFrame } from "@react-three/fiber";
 import Model from "../Model";
 import { type Medalist } from "~/pages/podium";
-import { Text3D, useTexture } from "@react-three/drei";
+import { Center, Text3D, useTexture } from "@react-three/drei";
+import { shortAccount } from "~/utils/addresses";
 
 type MedalistProps = {
   player: Medalist;
@@ -22,18 +23,28 @@ const Medalist = ({ player, medal }: MedalistProps) => {
     dinoNumber = "5630";
   }
 
+  const chunks = handle?.split(" ");
+  const displayName =
+    handle && handle?.length >= 10
+      ? chunks && chunks.length > 1
+        ? chunks[0]
+        : shortAccount(handle, 10)
+      : handle;
+
   return (
     <>
       <group position={[0, 0, 0]} castShadow>
-        <Text3D
-          scale={0.35}
-          font="/fonts/Titan_One_Regular.json"
-          position={[0.9, 1.75, 1]}
-          rotation={[0, Math.PI, 0]}
-        >
-          {handle}
-          <meshMatcapMaterial matcap={matcap} />
-        </Text3D>
+        <Center position={[0, 2, 0]}>
+          <Text3D
+            scale={0.35}
+            font="/fonts/Titan_One_Regular.json"
+            position={[1, 1.75, 1]}
+            rotation={[0, Math.PI, 0]}
+          >
+            {displayName}
+            <meshMatcapMaterial matcap={matcap} />
+          </Text3D>
+        </Center>
         <Model modelName={modelString} nftId={dinoNumber} />
       </group>
     </>
