8653152aeae35b01b426979417c4e824e2f71241
racollette <racollette@gmail.com>
2024-02-26 09:26:31 -0500

    fix

--- src/pages/delivery/index.tsx
+++ src/pages/delivery/index.tsx
@@ -35,7 +35,7 @@ function DeliveryEvent() {
         </Canvas>
         <Interface />
         <LoadingScreen
-          totalFiles={115}
+          totalFiles={110}
           started={start}
           startExperience={() => setStart(true)}
         />
